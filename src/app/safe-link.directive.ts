fb25edb93e5cdf010551fd5abb27b9be920adb8f
ipavlakos <pavlakosIlias@outlook.com>
2024-08-16 16:51:29 +0300

    Using attribute directives to change element behavior

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -3,10 +3,22 @@ import {Directive} from "@angular/core";
 @Directive({
   selector: 'a[appSaveLink]',
   standalone: true,
-
+  host: {
+    '(click)': 'onConfirmLeavePage($event)'
+  }
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('SafeLinkDirective');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const confirmed = window.confirm("Do you want to leave the app?");
+
+    if (confirmed) {
+      return;
+    }
+
+    event.preventDefault();
   }
 }
